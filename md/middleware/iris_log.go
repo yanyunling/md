0d77164dc6842809ec5927d296fa8994d7fedf42
陈圣朋 <shengpeng.chen@accuenergy.com.cn>
2023-05-09 17:22:44 +0800

    fix: 调整日志文件权限

--- md/middleware/iris_log.go
+++ md/middleware/iris_log.go
@@ -33,7 +33,7 @@ func InitLog(prefixPath string, logger *golog.Logger) {
 		}
 		// 生成日志文件
 		lastTime = time.Now().Format("20060102")
-		currentFile, err := os.OpenFile(prefixPath+lastTime+".log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0777)
+		currentFile, err := os.OpenFile(prefixPath+lastTime+".log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
 		if err != nil {
 			panic(err)
 		}
@@ -49,7 +49,7 @@ func InitLog(prefixPath string, logger *golog.Logger) {
 			currentTime := time.Now().Format("20060102")
 			// 时间不一致，则生成新的日志文件
 			if lastTime != currentTime {
-				currentFile, err := os.OpenFile(prefixPath+currentTime+".log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0777)
+				currentFile, err := os.OpenFile(prefixPath+currentTime+".log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
 				if err != nil {
 					Log.Error("日志文件创建失败：", err)
 					continue
