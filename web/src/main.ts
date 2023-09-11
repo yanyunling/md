6c96cb6db660c4006c41034844ce71f0de4308a6
chenshengpeng <1053544949@qq.com>
2023-09-11 17:18:49 +0800

    feat: 升级编辑器版本

--- web/src/main.ts
+++ web/src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from "vue";
 import "./styles/index.scss";
 import ElementPlus from "element-plus";
-import locale from "element-plus/lib/locale/lang/zh-cn";
+import locale from "element-plus/es/locale/lang/zh-cn";
 import App from "./App.vue";
 import router from "./router";
 import "virtual:svg-icons-register";
