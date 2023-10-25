2936fa79cbba1e27c6f2ea8e4759dfefdac5bc2e
chenshengpeng <1053544949@qq.com>
2023-10-25 16:21:49 +0800

    feat: json格式化改用codemirror

--- web/src/views/tool/components/json-format.vue
+++ web/src/views/tool/components/json-format.vue
@@ -1,30 +1,28 @@
 <template>
   <div class="page-json-format">
-    <el-input v-model="content" type="textarea" :rows="5" resize="none" placeholder="请输入JSON文本"></el-input>
-    <json-viewer class="json-view" :value="jsonContent" copyable sort previewMode expanded theme="light" />
+    <el-button type="primary" @click="formatClick">格式化</el-button>
+    <div class="json-view">
+      <codemirror-editor v-model="content" placeholder="请输入json" />
+    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { ref, watch } from "vue";
+import { ref } from "vue";
 import { jsonrepair } from "jsonrepair";
-import { JsonViewer } from "vue3-json-viewer";
-import "vue3-json-viewer/dist/index.css";
+import CodemirrorEditor from "@/components/codemirror-editor";
+import { ElMessage } from "element-plus";
 
 const content = ref("");
-const jsonContent = ref("");
 
-watch(
-  () => content.value,
-  (val) => {
-    try {
-      jsonContent.value = JSON.parse(jsonrepair(val));
-    } catch (e) {
-      jsonContent.value = val;
-      console.error(e);
-    }
+const formatClick = () => {
+  try {
+    content.value = JSON.stringify(JSON.parse(jsonrepair(content.value)), null, 4);
+  } catch (e) {
+    ElMessage.warning("json格式有误");
+    console.error(e);
   }
-);
+};
 </script>
 
 <style lang="scss">
@@ -33,12 +31,10 @@ watch(
   flex-direction: column;
   align-items: center;
   .json-view {
-    margin-top: 10px;
+    margin-top: 5px;
+    height: 80vh;
     width: 100%;
-    height: 70vh;
-    overflow: auto;
-    box-shadow: 0 0 0 1px #dcdfe6 inset;
-    border-radius: 6px;
+    border: 1px solid #ddd;
   }
 }
 </style>
